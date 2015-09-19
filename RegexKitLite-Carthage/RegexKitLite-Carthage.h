2c510d3aec9d8b54e560544afdff56370beac579
dyland <dyland@adobe.com>
2015-09-19 00:10:55 -0400

    Initial Commit

---
+++ RegexKitLite-Carthage/RegexKitLite-Carthage.h
@@ -0,0 +1,19 @@
+//
+//  RegexKitLite-Carthage.h
+//  RegexKitLite-Carthage
+//
+//  Created by Dylan DePass on 2015-09-18.
+//  Copyright © 2015 RegexKitLite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RegexKitLite-Carthage.
+FOUNDATION_EXPORT double RegexKitLite_CarthageVersionNumber;
+
+//! Project version string for RegexKitLite-Carthage.
+FOUNDATION_EXPORT const unsigned char RegexKitLite_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexKitLite_Carthage/PublicHeader.h>
+
+
